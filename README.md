06aadf3de029126247fb3b691ac6030ba4b84cdb
Felipe <felipedelite@gmail.com>
2022-11-30 13:19:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Estrutura do projeto
+
+- Descrever aqui como funciona a estrutura do projeto
+
+## Instruções para rodar o projeto
---
+++ server.js
@@ -0,0 +1,11 @@
+const net = require("net");
+const server = net.createServer((socket) => {
+  socket.write("Hello From Server!");
+  socket.on("data", (data) => {
+    console.log(data.toString());
+  });
+  socket.on("close", () => {
+    console.log("Connection closed.!!!");
+  });
+});
+server.listen(8080);
